557e00c679b437fe9b16da4e75160701210c8c63
ysk-tngc <ysk.tngc0505@gmail.com>
2021-01-15 20:00:32 +0900

    fix: typo

--- parser/parser.go
+++ parser/parser.go
@@ -17,13 +17,13 @@ func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
 	// 2つトークンを読み込む。curToken と peekToken の両方がセットされる
-	p.newToken()
-	p.newToken()
+	p.nextToken()
+	p.nextToken()
 
 	return p
 }
 
-func (p *Parser) newToken() {
+func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
