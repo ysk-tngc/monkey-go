37aa86c20c477d6d22c7cb4816b78b67aed81cee
ysk-tngc <ysk.tngc0505@gmail.com>
2021-02-11 21:16:27 +0900

    add: ExpressionStatement

--- ast/ast.go
+++ ast/ast.go
@@ -52,3 +52,11 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token token.Token // 式の最初のトークン
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
